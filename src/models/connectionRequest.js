aaf562f64062b13d613d3255cdd1f48e39b3c03b
Kushwanth12215773 <119439255+Kushwanth-004@users.noreply.github.com>
2025-07-18 14:24:42 +0530

    Update connectionRequest.js

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -33,7 +33,7 @@ connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
-  //check if the from user id and to id are same
+  //check if the from user id and to id are same are not
   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
     throw new Error(
       "from User Id and to User Id is same that is cannot send connection request to yourself"
